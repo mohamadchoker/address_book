60fbc9b283d8f64d88a428f81c3a29e9fab043e3
mohamadchoker <m.choker@live.com>
2021-01-23 17:58:56 +0200

    -add compiled assets using laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('public/sass/app.scss', 'public/css').version();
+mix.styles('public/css/custom.css', 'public/css/custom.css').version();
+mix.scripts([
+    'public/js/vendor/jquery.js',
+    'public/js/vendor/poper.js',
+    'public/js/vendor/bootstrap.js',
+    'public/js/vendor/jquery-migrate.js',
+    'public/js/vendor/jquery-ui.js',
+    'public/js/vendor/jquery-validate.js',
+    'public/js/vendor/jquery-scrollbar.js',
+    'public/js/vendor/jquery-touch-punch.js',
+    'public/js/vendor/noitify.js',
+    'public/js/vendor/sweetalert.js',
+    'public/js/vendor/lazyload.js',
+    'public/js/vendor/core.js'
+],'public/js/vendor.js').version();
+mix.scripts('public/js/dev.js', 'public/js/app.js').version();
